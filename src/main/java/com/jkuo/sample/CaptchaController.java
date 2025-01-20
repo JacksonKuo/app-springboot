6d115b230d3a4784edc57d39d5e9404583d55c0c
JacksonKuo <JacksonKuo@users.noreply.github.com>
2025-01-19 23:18:07 -0600

    captcha endpoint

---
+++ src/main/java/com/jkuo/sample/CaptchaController.java
@@ -0,0 +1,15 @@
+package com.jkuo.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/captcha")
+public class CaptchaController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Captcha";
+	}
+
+}
