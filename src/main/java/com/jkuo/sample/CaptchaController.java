23c5c64508f10a0fd0ffc7ff40775deded5eeff2
JacksonKuo <JacksonKuo@users.noreply.github.com>
2025-01-20 00:13:08 -0600

    captcha endpoint

--- src/main/java/com/jkuo/sample/CaptchaController.java
+++ src/main/java/com/jkuo/sample/CaptchaController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping("/captcha" produces = "text/html")
+@RequestMapping("/captcha", produces = "text/html")
 public class CaptchaController {
 
 	@GetMapping("")
